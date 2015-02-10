500abb2efd3f3068da3855bf9de36a2784f9a302
Hunter Loftis <hunter@hunterloftis.com>
2015-02-10 09:29:25 -0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,32 @@
+# Node Summit 15
+
+## Production Node Architecture
+
+Clone the project:
+
+```
+git clone https://github.com/heroku-examples/summit-service.git
+cd summit-service
+npm install
+
+  Windows:
+
+```
+
+Inject config (Linux / OSX):
+
+```
+export CLOUDAMQP_URL=amqp://idhertwy:FRHI5wRxFrZJWDF4eI2BjPB7_qTyUEHh@fast-coyote.rmq.cloudamqp.com/idhertwy
+```
+
+Inject config (Windows):
+
+```
+set CLOUDAMQP_URL=amqp://idhertwy:FRHI5wRxFrZJWDF4eI2BjPB7_qTyUEHh@fast-coyote.rmq.cloudamqp.com/idhertwy
+```
+
+Start the service locally:
+
+```
+npm start
+```
---
+++ service.js
@@ -0,0 +1,46 @@
+// Modules
+
+var logger = require('logfmt');
+var jackrabbit = require('jackrabbit');
+var weather = require('weather-js');
+
+// Config
+
+var RABBIT_URL = process.env.CLOUDAMQP_URL || 'amqp://localhost';
+var SERVICE = 'weather.get';
+
+// Startup
+
+logger.log({ type: 'info', message: 'starting' });
+
+var broker = jackrabbit(RABBIT_URL, 1)
+  .once('connected', onBroker)
+  .once('disconnected', onBrokerLost);
+
+// Callbacks
+
+function onBroker() {
+  logger.log({ type: 'info', message: 'connected', service: 'broker' });
+  broker.create(SERVICE, register);
+}
+
+function register() {
+  logger.log({ type: 'info', message: 'registering', service: SERVICE });
+  broker.handle(SERVICE, getWeather);
+}
+
+function getWeather(message, respond) {
+  logger.log({ type: 'info', message: 'request', service: SERVICE });
+  weather.find({ search: message.zip, degreeType: 'F' }, function onWeather(err, result) {
+    var temp = result && result[0] && result[0].current.temperature;
+    var ok = !err && result;
+    var res = ok ? "It's currently " + temp + " degrees in " + message.zip : undefined;
+    logger.log({ type: 'info', message: 'response', service: SERVICE, response: res });
+    respond(res);
+  });
+}
+
+function onBrokerLost() {
+  loggerl.log({ type: 'error', message: 'disconnected', service: 'broker' });
+  process.exit();
+}
